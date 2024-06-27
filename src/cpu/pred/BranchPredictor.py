80118df80a7e4b86955018da5cd7f7d83e600301
arefzarezadeh <arefzarezade@gmail.com>
2024-06-27 16:21:31 +0330

    theta = 16 or

--- src/cpu/pred/BranchPredictor.py
+++ src/cpu/pred/BranchPredictor.py
@@ -163,7 +163,7 @@ class PerceptronBP(BranchPredictor):
 
     count_perc = Param.Unsigned(8, "number of perceptrons")
     n = Param.Unsigned(15, "number of weights in each perceptron")
-    theta = Param.Unsigned(5, "the threshold")
+    theta = Param.Unsigned(16, "the threshold")
 
 class LocalBP(BranchPredictor):
     type = "LocalBP"
